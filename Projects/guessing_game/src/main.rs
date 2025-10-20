7ad442df919ead87d1d06307e4981c922614fc57
HZNPrince <hznprincework@gmail.com>
2025-10-20 17:35:29 +0530

    The Rust Book documentation

---
+++ Projects/guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+use std::{io, cmp::Ordering};
+use rand::Rng;
+
+fn main() {
+    println!("Guessing the Number!");
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is {secret_number}");
+
+    loop{
+        println!("Enter the Number to guess");
+
+        let mut guess = String::new();
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number \n");
+                continue;
+            },
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            },
+        }
+    }
+}
